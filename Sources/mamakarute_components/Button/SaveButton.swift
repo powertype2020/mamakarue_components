462a364fe3e2ad08643892e36fe101fa56e72d0d
Naoki Takehisa <zohysama@icloud.com>
2023-01-21 18:54:44 +0900

    軽微な修正

--- Sources/mamakarute_components/Button/SaveButton.swift
+++ Sources/mamakarute_components/Button/SaveButton.swift
@@ -29,21 +29,70 @@ public struct SaveButton: View {
         }
     }
     
-    public enum SaveButtonTextSize: CGFloat {
+    public enum SaveButtonRectangleSize: CaseIterable {
+        /// width大.
+        case largeW
+        /// width中.
+        case mediumW
+        /// width小.
+        case smallW
+        /// height大.
+        case largeH
+        /// height中.
+        case mediunH
+        /// height小.
+        case smallH
+        
+        public var size: CGFloat {
+            switch self {
+            case .largeW:
+                return 200
+            case .mediumW:
+                return 100
+            case .smallW:
+                return 50
+            case .largeH:
+                return 50
+            case .mediunH:
+                return 25
+            case .smallH:
+                return 12.5
+            }
+        }
+    }
+    
+    public enum SaveButtonTextSize: CaseIterable {
         /// 大.
-        case large = 60
+        case large
         /// 中.
-        case medium = 30
+        case medium
         /// 小.
-        case small = 15
+        case small
+        
+        public var size: CGFloat {
+            switch self {
+            case .large:
+                return 60
+            case .medium:
+                return 30
+            case .small:
+                return 15
+            }
+        }
     }
     
     /// 保存ボタンのラベル.
     private let label: String
     /// 保存ボタンのカラー.
     private let buttonColor: SaveButtonColor
+    /// ボタンテキストのサイズ.
+    private let textSize: SaveButtonTextSize
     /// Rectangleのカラー.
     private let rectangleColor: SaveButtonColor
+    /// RectangleのWidth.
+    private let rectangleWidth: SaveButtonRectangleSize
+    /// Rectangleのheight.
+    private let rectangleHeight: SaveButtonRectangleSize
     /// 保存ボタンのRectangleのフラグ.
     private let withRectangle: Bool
     /// 保存ボタンのアクション.
@@ -52,13 +101,19 @@ public struct SaveButton: View {
     public init(
         label: String,
         buttonColor: SaveButtonColor,
+        textSize: SaveButtonTextSize,
         rectangleColor: SaveButtonColor,
+        rectangleWidth: SaveButtonRectangleSize,
+        rectangleHeight: SaveButtonRectangleSize,
         withRectangle: Bool,
         action: @escaping () -> Void
     ) {
         self.label = label
         self.buttonColor = buttonColor
+        self.textSize = textSize
         self.rectangleColor = rectangleColor
+        self.rectangleWidth = rectangleWidth
+        self.rectangleHeight = rectangleHeight
         self.withRectangle = withRectangle
         self.action = action
     }
@@ -69,10 +124,12 @@ public struct SaveButton: View {
                 RoundedRectangle(cornerRadius: 5)
                     .stroke(buttonColor.color)
                     .foregroundColor(rectangleColor.color)
+                    .frame(width: rectangleWidth.size, height: rectangleHeight.size)
             }
             Button(action: action) {
                 Text(verbatim: label)
                     .foregroundColor(buttonColor.color)
+                    .font(.system(size: textSize.size))
             }
         }
     }
